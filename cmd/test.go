3eee98c81e09b3ad093cfa157cd8bd0ec239a4c2
samanar <ahmadianrad.saman@gmail.com>
2025-10-27 20:19:16 +0330

    reading chunks added

--- cmd/test.go
+++ cmd/test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/samanar/clai/man"
 	"github.com/spf13/cobra"
@@ -11,14 +12,34 @@ var testCmd = &cobra.Command{
 	Use:   "test",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		man, err := man.NewMan()
+		m, err := man.NewMan()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(man.RootPaths)
+		fmt.Println(m.RootPaths)
 		fmt.Println("-----------------------------------")
-		fmt.Println(len(man.ManFiles))
-		fmt.Println(man.ManFiles[0])
+		fmt.Println(len(m.ManFiles))
+		fmt.Println(m.ManFiles[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error listing man files: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Found %d man files across %d paths\n", len(m.ManFiles), len(m.RootPaths))
+
+		const chunkSize = 64 * 1024
+		for _, f := range m.ManFiles {
+			for ch := range man.ReadManFileInChunks(f, chunkSize) {
+				if ch.Err != nil {
+					fmt.Fprintf(os.Stderr, "read error on %s: %v\n", ch.Path, ch.Err)
+					break
+				}
+				preview := ch.Data
+				if len(preview) > 80 {
+					preview = preview[:80]
+				}
+				fmt.Printf("[%s (sec %s)] chunk %d: %q\n", ch.Path, ch.Section, ch.Index, string(preview))
+			}
+		}
 	},
 }
 
